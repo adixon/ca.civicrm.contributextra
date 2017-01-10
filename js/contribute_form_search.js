7a89ede1c8c1eeabea4a0482c2561adb91838a46
adixon <alan.g.dixon@gmail.com>
2017-01-10 15:15:35 -0500

    fix for html changes in 4.7

--- js/contribute_form_search.js
+++ js/contribute_form_search.js
@@ -13,7 +13,7 @@ cj(function ($) {
     var backofficeLinks = (typeof CRM.vars.contributextra != 'undefined') ? CRM.vars.contributextra.backofficeLinks : CRM.contributextra.backofficeLinks;
     if (0 < backofficeLinks.length) {
       $.each(backofficeLinks, function(index, value) {
-         $('form#Search #help').after(' <a style="color: #F88;" class="button" href="'+value.url+'">'+value.title+'</a>');
+         $('form#Search').find('#help, .help').after(' <a style="color: #F88;" class="button" href="'+value.url+'">'+value.title+'</a>');
       });
     }
   });
